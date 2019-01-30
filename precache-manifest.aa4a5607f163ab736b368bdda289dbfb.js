f7ff62eae7bc7e3699a36336b2ab566ff4d715dc
Parinya <parinya@Parinyas-MacBook-Pro.local>
2019-01-30 16:27:35 +0700

    Updates

---
+++ precache-manifest.aa4a5607f163ab736b368bdda289dbfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c397073d2a3ee052c0e6",
+    "url": "/resistor-calculator/static/js/runtime~main.c397073d.js"
+  },
+  {
+    "revision": "67ee2321dca5dd39ae2d",
+    "url": "/resistor-calculator/static/js/main.67ee2321.chunk.js"
+  },
+  {
+    "revision": "565cd0f5e094528aa74f",
+    "url": "/resistor-calculator/static/js/1.565cd0f5.chunk.js"
+  },
+  {
+    "revision": "67ee2321dca5dd39ae2d",
+    "url": "/resistor-calculator/static/css/main.784ab4a3.chunk.css"
+  },
+  {
+    "revision": "99c2ef99f9affa8684dd1a0f06096a6a",
+    "url": "/resistor-calculator/index.html"
+  }
+];
